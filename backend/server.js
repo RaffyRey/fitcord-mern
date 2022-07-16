6054c82610638b8a6b63b5503fac51f4d36a3d3b
RaffyRey <raffyquemadarey30@gmail.com>
2022-07-16 09:09:13 +0800

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const dotenv = require('dotenv').config()
 const express = require('express')
 const colors = require('colors')
@@ -19,6 +20,15 @@ app.use(express.urlencoded({ extended: true }))
 // getting the routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/workout', require('./routes/workoutRoutes'))
+
+// serve front end
+if(process.env.NODE_ENV === 'production') {
+ app.use(express.static(path.join(__dirname, '../client/build')))
+
+ app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "../", "client", "build","index.html")))
+} else {
+ app.get("/", (req,res) => res.send("Please set to production"))
+}
 
 // error middleware
 app.use(errorHandler)
