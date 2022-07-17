6f4c6ea8aa71382b245b63673815b7551260a883
RaffyRey <raffyquemadarey30@gmail.com>
2022-07-17 10:05:35 +0800

    Just added a little touches in login form

--- client/src/style/container.js
+++ client/src/style/container.js
@@ -1,54 +1,57 @@
 import styled from 'styled-components';
 
 export const PageContainer = styled.div`
- width: 100%;
- height: 100vh;
- overflow: auto;
- display: block;
- position: relative;
- margin: 0;
- padding: 0;
+	width: 100%;
+	height: 100vh;
+	overflow: auto;
+	display: block;
+	position: relative;
+	margin: 0;
+	padding: 0;
 `;
 
 export const PageContent = styled.main`
- width: 100%;
- max-width: 1440px;
- padding-bottom: 30px;
- margin: 0 auto;
- position: relative;
- display: block;
+	width: 100%;
+	max-width: 1440px;
+	padding-bottom: 30px;
+	margin: 0 auto;
+	position: relative;
+	display: block;
 `;
 
 export const Form = styled.form`
- width: 100%;
- height: 100%;
- display: flex;
- flex-direction: column;
- align-items: center;
- overflow: auto;
+	max-width: 1000px;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin: 0 auto;
+	padding-bottom: 2rem;
+	overflow: auto;
 
- span{
-  font-size: .9rem;
-  margin: 1rem 0;
-  font-family: 'Poppins';
-  color: #00193b;
- }
+	span {
+		font-size: 0.9rem;
+		margin: 1rem 0;
+		font-family: 'Poppins';
+		color: #00193b;
+	}
 
- a{
-  font-size: .9rem;
-  font-style: normal;
-  font-weight: 500;
-  text-decoration: none;
-  color: #0086eb;
+	a {
+		font-size: 0.9rem;
+		font-style: normal;
+		font-weight: 500;
+		text-decoration: none;
+		color: #0086eb;
 
-  &:hover{
-   text-decoration: underline;
-  }
- }
+		&:hover {
+			text-decoration: underline;
+		}
+	}
 `;
 
 export const DashboardContent = styled.div`
- width: 100%;
- height: calc(100% - 80px);
- position: relative;
+	width: 100%;
+	height: calc(100% - 80px);
+	position: relative;
 `;
